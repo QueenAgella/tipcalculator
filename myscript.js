71b5790019029025e94f2e23102cad1c0f2fa747
QueenAgella <hervinfakhrulm@gmail.com>
2021-03-10 05:03:36 +0700

    TIP KALKULATOR

---
+++ myscript.js
@@ -0,0 +1,15 @@
+function calculate() {
+    var amount = $('#amount').val();
+    var percentage = $('#Tip').val();
+    var tip = amount * (percentage / 100);
+    var total = Number(amount) + tip;
+
+    $('#tipamount').val(tip.toFixed(2));
+    $('#totalamount').val(total.toFixed(2));
+
+    return false;
+}
+
+$('#calculator').submit(calculate);
+
+// COPYRIGHT HERVIN FAKHRUL
